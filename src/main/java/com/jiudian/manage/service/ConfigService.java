e476f4df4c90a95299241dde49c59c6ef551005e
justice <1025974653@qq.com>
2022-03-18 10:33:55 +0800

    第一次导入

--- src/main/java/com/jiudian/manage/service/ConfigService.java
+++ src/main/java/com/jiudian/manage/service/ConfigService.java
@@ -5,5 +5,6 @@ import com.jiudian.manage.model.Config;
 
 public interface ConfigService {
     public Config get();
+
     public boolean update(double managesalary,double  staffsalary, double cleanerssalary, double manage, double staff, double cleaner, double totalmoney, double totalroom);
 }
